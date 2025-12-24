486852be7e5686e0931779c816593931b6eb5e78
Ithea Valgulious <itheamyse@outlook.com>
2025-12-24 16:29:28 +0800

    Change base URL from 'boat_design_homework1' to 'english_homework1'

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+ishimport path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -19,6 +19,6 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       },
-        base: "boat_design_homework1"
+        base: "english_homework1"
     };
 });
